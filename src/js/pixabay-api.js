94e7b6ebd9b1790de214387db1f3c2e035096ceb
Kondratiuk2023 <149534389+Kondratiuk2023@users.noreply.github.com>
2024-04-11 22:53:36 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,5 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+// import iziToast from "izitoast";
+// import "izitoast/dist/css/iziToast.min.css";
 
 const axios = require('axios');
 const apiKey = '43346019-e2cdab7ada109c45f6a1c5d2a';
