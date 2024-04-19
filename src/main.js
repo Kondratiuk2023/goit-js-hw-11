f6ed84fca02febc977279ac1decf815a7142553b
Kondratiuk2023 <149534389+Kondratiuk2023@users.noreply.github.com>
2024-04-19 13:35:57 +0300

    now

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-
-
 import { searchImages } from './js/pixabay-api.js';
 import { renderGallery } from './js/render-functions.js';
 
@@ -8,26 +6,28 @@ import "izitoast/dist/css/iziToast.min.css";
 
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+// import 'css-loader/dist/css-loader.css';
 
 const book = new SimpleLightbox('.list_gallery a');
 
 const form = document.querySelector('.main_form');
 const myGallery = document.querySelector('.list_gallery');
 
-const loadEl = document.querySelector('.loader');
-
 form.addEventListener("submit", handlerForm);
 
 function handlerForm(event) {
     event.preventDefault();
-    loadEl.classList.add('loading');
     myGallery.innerHTML = '';
+    const loader = document.querySelector('.loader');
+    loader.classList.add('loading');
+    
     let searchWord = event.currentTarget.elements.inputElement.value;
-
+    
     event.currentTarget.elements.inputElement.value = "";  
-
+    
     searchImages(searchWord)
-        .then(data => {
+    .then(data => {
+
             if (data.total == 0) {
                 iziToast.show({
                     title: 'Ops.',
@@ -43,6 +43,7 @@ function handlerForm(event) {
             } else {
                 myGallery.insertAdjacentHTML('beforeend', renderGallery(data));
                 book.refresh();
+                event.target.reset();
             }
         })
         .catch(error => {
@@ -57,6 +58,6 @@ function handlerForm(event) {
             });
         })
         .finally(() => { 
-            loadEl.classList.remove('loading')
-        });
+            loader.classList.remove('loading')
+                });
 }
